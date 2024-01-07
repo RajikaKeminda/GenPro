96ec7e7dc41472d2852de76cf7cafc79e02417c1
Rajika keminda <rkeminda0@gmail.com>
2024-01-07 11:08:47 +0530

    fix:setup

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name='pbot',
     version='0.1.0',
+    author="Rajika Keminda",
+    author_email="rkeminda0@gmail.com",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click', 'python-dotenv', 'rich', 'openai'
+        'click', 'python-dotenv', 'rich', 'openai'
     ],
+    keywords=["cli", "AI", "python", "project manager"],
     entry_points={
         'console_scripts': [
             'pbot = pbot.commands:cli',
