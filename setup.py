58bac4ce5aea513aff1c651adccb01bf7528bd94
Rajika keminda <rkeminda0@gmail.com>
2024-01-07 10:52:13 +0530

    fix:requirenments

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click',
+        'Click', 'python-dotenv', 'rich', 'openai'
     ],
     entry_points={
         'console_scripts': [
