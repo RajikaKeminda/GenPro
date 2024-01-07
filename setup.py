35a03aab4efcdcf3191093c5da259cb6b802ec80
Rajika keminda <rkeminda0@gmail.com>
2024-01-07 19:51:33 +0530

    feat:version-0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='genpro',
-    version='0.1.1',
+    version='0.1.2',
     author="Rajika Keminda",
     author_email="rkeminda0@gmail.com",
     packages=find_packages(),
